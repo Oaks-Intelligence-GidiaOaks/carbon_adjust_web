8a712fc9ebad1cf538e4155e03822a29fdfcdee8
eotuonye <emmanuel.otuonye@dlhosolutions.ng>
2024-04-10 19:21:48 +0100

    package form added

--- src/pages/protected/finance/Packages.tsx
+++ src/pages/protected/finance/Packages.tsx
@@ -1,8 +1,14 @@
 // import React from "react";
 
+import NoPackages from "@/components/reusables/NoPackages";
+
 // type Props = {};
 
 const Packages = () => {
+  if (true) {
+    return <NoPackages route="/finance/packages/add" />;
+  }
+
   return <div>Packages</div>;
 };
 
--- src/pages/protected/hia/Packages.tsx
+++ src/pages/protected/hia/Packages.tsx
@@ -1,7 +1,50 @@
-// import React from 'react'
+import { Grid } from "@/components/grid";
+import NoPackages from "@/components/reusables/NoPackages";
+import { Button } from "@/components/ui";
+import pkgsData from "../../../dummy/pkgData.json";
+import HiaPackageCard from "@/components/ui/HiaPackageCard";
+import { Link } from "react-router-dom";
 
 const Packages = () => {
-  return <div>Packages</div>;
+  if (false) {
+    return <NoPackages route="/hia/package/add" />;
+  }
+
+  return (
+    <div className="font-poppins">
+      <div className="flex-center justify-between">
+        <div>
+          <h2 className="page-header">Packages</h2>
+          <p className="text-=[667085] text-sm ">
+            Here are your recently created packages
+          </p>
+        </div>
+
+        <Link to="/hia/packages/add">
+          <Button className=" text-white flex-center gap-3">
+            <span className="text-white">Create package</span>
+            <img
+              src="/assets/icons/plus-circle.svg"
+              className=""
+              alt="carbon adjust icon"
+            />
+          </Button>
+        </Link>
+      </div>
+
+      {/* cards */}
+      <div className="flex-center border overflow-x-scroll gap-3 mt-[40px]">
+        <HiaPackageCard data={[]} />
+        <HiaPackageCard data={[]} />
+        <HiaPackageCard data={[]} />
+      </div>
+
+      {/* table */}
+      <div className="mt-[40px]">
+        <Grid data={pkgsData} tableStyles={` `} pageSize={40} />
+      </div>
+    </div>
+  );
 };
 
 export default Packages;
